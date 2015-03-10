db5ffef3644d6acdaa0980e77c0eb406805117ca
Matt Spoon <mspoon@gmail.com>
2015-03-10 01:38:14 -0400

    Moved all code back into main.rs for now

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,66 @@
-extern crate blackjack;
+#[allow(dead_code)]
+pub enum Value {
+    Ace,
+    Two,
+    Three,
+    Four,
+    Five,
+    Six,
+    Seven,
+    Eight,
+    Nine,
+    Ten,
+    Jack,
+    Queen,
+    King,
+}
 
-use blackjack::deck_of_cards;
+#[allow(dead_code)]
+pub enum Suit {
+    Clubs,
+    Diamonds,
+    Hearts,
+    Spades,
+}
+
+//type Card = (Value, Suit);
+
+//type Deck = Vec<Card>;
+
+impl ToString for Value {
+    fn to_string(&self) -> String {
+        match *self {
+            Value::Ace => "A",
+            Value::Two => "2",
+            Value::Three => "3",
+            Value::Four => "4",
+            Value::Five => "5",
+            Value::Six => "6",
+            Value::Seven => "7",
+            Value::Eight => "8",
+            Value::Nine => "9",
+            Value::Ten => "10",
+            Value::Jack => "J",
+            Value::Queen => "Q",
+            Value::King => "K",
+        }.to_string()
+    }
+}
+
+impl ToString for Suit {
+    fn to_string(&self) -> String {
+        match *self {
+            Suit::Clubs => "♣",
+            Suit::Diamonds => "♦",
+            Suit::Hearts => "♥",
+            Suit::Spades => "♠",
+        }.to_string()
+    }
+}
 
 fn main() {
-    let value = deck_of_cards::Value::Two;
-    let suit = deck_of_cards::Suit::Hearts;
+    let value = Value::Two;
+    let suit = Suit::Hearts;
 
     println!("{}{}", value.to_string(), suit.to_string());
 }
